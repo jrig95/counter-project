6e1f489d186a7027f22e859ad38aa9cccebd1640
James Riggleman <james.riggleman@yahoo.com>
2021-03-22 22:06:16 +0800

    newest version

--- counter.js
+++ counter.js
@@ -1,16 +1,15 @@
- var clicks = 0;
+ let counter = 0;
 
     function up() {
-        clicks += 1;
-        document.getElementById("clicks").innerHTML = clicks;
-        console.log((clicks < 5) ? 'keep going!' : 'go back!')
+        counter += 1;
+        document.getElementById("clicker").innerHTML = counter;
+        console.log((counter < 5) ? 'keep going!' : 'go back!')
         // clicks > 5 ? console.log("Go back!") : console.log("Keep going!");
     };
 
- var clicks = 0;
     function down() {
-        clicks -= 1;
-        document.getElementById("clicks").innerHTML = clicks;
-        console.log((clicks < -5) ? 'go back!' : 'keep going!')
+        counter -= 1;
+        document.getElementById("clicker").innerHTML = counter;
+        console.log((counter < -5) ? 'go back!' : 'keep going!')
         // clicks > -5 ? console.log("Go back!") : console.log("Keep going!");
     };
