9e9b8d08e300e7d9235aeb3841354c781dbaa887
James Riggleman <james.riggleman@yahoo.com>
2021-03-23 14:03:58 +0800

    updated javascript code new

--- counter.js
+++ counter.js
@@ -1,15 +1,38 @@
  let counter = 0;
+ const clicker = document.querySelector("#clicker");
+const buttonUp = document.querySelector("#up");
+const buttonDown = document.querySelector("#down");
 
-    function up() {
-        counter += 1;
-        document.getElementById("clicker").innerHTML = counter;
-        console.log((counter < 5) ? 'keep going!' : 'go back!')
-        // clicks > 5 ? console.log("Go back!") : console.log("Keep going!");
-    };
 
-    function down() {
-        counter -= 1;
-        document.getElementById("clicker").innerHTML = counter;
-        console.log((counter < -5) ? 'go back!' : 'keep going!')
-        // clicks > -5 ? console.log("Go back!") : console.log("Keep going!");
-    };
+if (buttonUp != null) {
+	buttonUp.addEventListener("click", () => {
+		counter++;
+		clicker.innerHTML = counter;
+		counter > 5 ? console.log("go back!") : console.log("keep going!");
+	})
+}
+
+
+if (buttonDown != null) {
+	buttonDown.addEventListener("click", () => {
+		counter--;
+		clicker.innerHTML = counter;
+		
+		counter < -5 ? console.log("go back!") : console.log("keep going!")
+	})
+}
+
+
+    // function up() {
+    //     counter += 1;
+    //     document.getElementById("clicker").innerHTML = counter;
+    //     console.log((counter < 5) ? 'keep going!' : 'go back!')
+    //     // clicks > 5 ? console.log("Go back!") : console.log("Keep going!");
+    // };
+
+    // function down() {
+    //     counter -= 1;
+    //     document.getElementById("clicker").innerHTML = counter;
+    //     console.log((counter < -5) ? 'go back!' : 'keep going!')
+    //     // clicks > -5 ? console.log("Go back!") : console.log("Keep going!");
+    // };
